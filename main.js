86af31b9871fe251e7fed674fba10e6ae7e3a86c
jeevashanmugam <jeevasj2000@gmail.com>
2023-07-25 21:38:55 +0530

    JS-math-object

---
+++ main.js
@@ -0,0 +1,18 @@
+
+// console.log(Math.round(4.5));
+
+// console.log(Math.floor(4.7));
+
+// console.log(Math.ceil(4.7));
+
+//console.log(Math.pow(4,2));
+
+//console.log(Math.sign(25));
+
+//console.log(Math.abs(-25));
+
+//console.log(Math.random(25));
+
+console.log(Math.floor(Math.random()*(6-1)+1));
+
+console.log(Math.E)
